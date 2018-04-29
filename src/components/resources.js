73bb47a6c34bdab4650c0b10c41817218cd5f33a
glenrage <phamgalang@gmail.com>
2018-04-28 22:36:25 -0700

    finished HOC

---
+++ src/components/resources.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      secret compoennet
+      <ul>
+        <li>stiff</li>
+        <li>stiff</li>
+        <li>stiff</li>
+        <li>stiff</li>
+      </ul>
+    </div>
+  )
+}
