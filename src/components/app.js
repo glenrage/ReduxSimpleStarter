0c65b3eb06e1d61f5395b412d181811468930ba3
glenrage <phamgalang@gmail.com>
2018-05-01 15:06:55 -0700

    added comment box tests

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import Header from './header'
+import { connect } from 'react-redux';
+import Header from './header';
+import CommentBox from './comment_box';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Header />
-        {this.props.children}
+        <CommentBox />
       </div>
     );
   }
--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -2,13 +2,35 @@ import { renderComponent, expect } from '../test_helper';
 import CommentBox from '../../src/components/comment_box';
 
 describe('Comment Box', () => {
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(CommentBox);
+  });
+
+  it('has the correct class', () => {
+    expect(component).to.have.class('comment-box');
+  });
+
   it('has a text area', () => {
-    const component = renderComponent(CommentBox);
     expect(component.find('textarea')).to.exist;
   });
 
   it('has a button', () => {
-    const component = renderComponent(CommentBox);
     expect(component.find('button')).to.exist;
+  });
+
+  describe('entering some text', () => {
+    beforeEach(() => {
+      component.find('textarea').simulate('change', 'new comment');
+    });
+    it('shows text in textarea', () => {
+      expect(component.find('textarea')).to.have.value('new comment');
+    });
+
+    it('when submitted, clears the input', () => {
+      component.simulate('submit');
+      expect(component.find('textarea')).to.have.value('');
+    });
   });
 });
